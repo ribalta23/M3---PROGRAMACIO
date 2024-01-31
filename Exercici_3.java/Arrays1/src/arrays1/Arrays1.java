6edf5c25d2540ff1e80c5f02621a0c57aba46095
ribalta23 <aleixribalta04@gmail.com>
2024-01-31 11:05:23 +0100

    UPDATED

--- Exercici_3.java/Arrays1/src/arrays1/Arrays1.java
+++
@@ -1,9 +0,0 @@
-package arrays1;
-public class Arrays1 {
-    public static void main(String[] args) {
-        int[] vector = new int[5];
-        for(int i = 0; i < vector.length;i++){
-            System.out.print(vector[i]+" ");
-        }
-    }
-}
--- PSEUDOCODI/HolaMundo.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class HolaMundo {
-	public static void main(String[] args) {
-        
-                
-	}
-}
